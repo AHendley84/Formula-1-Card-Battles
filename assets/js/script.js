e5f13c9dc2ae2f7b7bfabb07d797c51febe306e8
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2023-08-10 17:31:05 +0000

    update card deck object in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,286 @@
+ /*jshint esversion: 6 */
+
 // Driver Card Deck
 
 const driverCardDeck = [
-    {driverName: "Max Verstappen", teamName: "Red Bull Racing", nationality: "Dutch", carNumber: 1, careerPodiums: 87, careerPoints: 2266.5, gpEntered: 173, raceWins: 43, driverChampionships: 2, img1: 'verstappen.png', img2: 'redbull.jpg'},
-    {driverName: "Sergio Perez", teamName: "Red Bull Racing", nationality: "Mexican", carNumber: 11, careerPodiums: 31, careerPoints: 1357, gpEntered: 246, raceWins: 6, driverChampionships: 0, img1: 'perez.png', img2: 'redbull.jpg'},
-    {driverName: "Lewis Hamilton", teamName: "Mercedes", nationality: "British", carNumber: 44, careerPodiums: 195, careerPoints: 4526.5, gpEntered: 320, raceWins: 103, driverChampionships: 7, img1: 'hamilton.png', img2: 'mercedes.jpg'},
-    {driverName: "George Russell", teamName: "Mercedes", nationality: "British", carNumber: 63, careerPodiums: 10, careerPoints: 376, gpEntered: 92, raceWins: 1, driverChampionships: 0, img1: 'russell.png', img2: 'mercedes.jpg'},
-    {driverName: "Charles Leclerc", teamName: "Ferrari", nationality: "Monegasque", carNumber: 16, careerPodiums: 26, careerPoints: 942, gpEntered: 113, raceWins: 5, driverChampionships: 0, img1: 'leclerc.png', img2: 'ferrari.jpg'},
-    {driverName: "Carlos Sainz", teamName: "Ferrari", nationality: "Spanish", carNumber: 55, careerPodiums: 15, careerPoints: 865.5, gpEntered: 173, raceWins: 1, driverChampionships: 0, img1: 'sainz.png', img2: 'ferrari.jpg'},
-    {driverName: "Fernando Alonso", teamName: "Aston Martin", nationality: "Spanish", carNumber: 14, careerPodiums: 104, careerPoints: 2198, gpEntered: 368, raceWins: 32, driverChampionships: 2, img1: 'alonso.png', img2: 'aston_martin.jpg',},
-    {driverName: "Lance Stroll", teamName: "Aston Martin", nationality: "Canadian", carNumber: 18, careerPodiums: 3, careerPoints: 238, gpEntered: 132, raceWins: 0, driverChampionships: 0, img1: 'stroll.png', img2: 'aston_martin.jpg'},
-    {driverName: "Lando Norris", teamName: "McLaren", nationality: "British", carNumber: 4, careerPodiums: 7, careerPoints: 470, gpEntered: 92, raceWins: 0, driverChampionships: 0, img1: 'norris.png', img2: 'mclaren.jpg'},
-    {driverName: "Oscar Piastri", teamName: "McLaren", nationality: "Australian", carNumber: 81, careerPodiums: 0, careerPoints: 17, gpEntered: 10, raceWins: 0, driverChampionships: 0, img1: 'piastri.png', img2: 'mclaren.jpg'},
-    {driverName: "Esteban Ocon", teamName: "Alpine", nationality: "French", carNumber: 31, careerPodiums: 3, careerPoints: 395, gpEntered: 121, raceWins: 1, driverChampionships: 0, img1: 'ocon.png', img2: 'alpine.jpg'},
-    {driverName: "Pierre Gasly", teamName: "Alpine", nationality: "French", carNumber: 10, careerPodiums: 3, careerPoints: 348, gpEntered: 118, raceWins: 1, driverChampionships: 0, img1: 'gasly.png', img2: 'alpine.jpg'},
-    {driverName: "Alexander Albon", teamName: "Williams", nationality: "Thai", carNumber: 23, careerPodiums: 2, careerPoints: 212, gpEntered: 69, raceWins: 0, driverChampionships: 0, img1: 'albon.png', img2: 'williams.jpg'},
-    {driverName: "Logan Sargeant", teamName: "Williams", nationality: "American", carNumber: 2, careerPodiums: 0, careerPoints: 0, gpEntered: 10, raceWins: 0, driverChampionships: 0, img1: 'sargeant.png', img2: 'williams.jpg'},
-    {driverName: "Valterri Bottas", teamName: "Alfa Romeo", nationality: "Finnish", carNumber: 77, careerPodiums: 67, careerPoints: 1792, gpEntered: 210, raceWins: 10, driverChampionships: 0, img1: 'bottas.png', img2: 'alfa_romeo.jpg'},
-    {driverName: "Zhou Guanyu", teamName: "Alfa Romeo", nationality: "Chinese", carNumber: 24, careerPodiums: 0, careerPoints: 10, gpEntered: 32, raceWins: 0, driverChampionships: 0, img1: 'zhou.png', img2: 'alfa_romeo.jpg'},
-    {driverName: "Nico Hulkenberg", teamName: "Haas F1 Team", nationality: "German", carNumber: 27, careerPodiums: 0, careerPoints: 530, gpEntered: 194, raceWins: 0, driverChampionships: 0, img1: 'hulkenberg.png', img2: 'haas.jpg'},
-    {driverName: "Kevin Magnussesn", teamName: "Haas F1 Team", nationality: "Danish", carNumber: 20, careerPodiums: 1, careerPoints: 185, gpEntered: 152, raceWins: 2, driverChampionships: 0, img1: 'magnussen.png', img2: 'haas.jpg'},
-    {driverName: "Yuki Tsunoda", teamName: "AlphaTauri", nationality: "Japanese", carNumber: 22, careerPodiums: 0, careerPoints: 46, gpEntered: 54, raceWins: 0, driverChampionships: 0, img1: 'tsunoda.png', img2: 'alpha_tauri.jpg'},
-    {driverName: "Daniel Ricciardo", teamName: "AlphaTauri", nationality: "Australian", carNumber: 3, careerPodiums: 32, careerPoints: 1311, gpEntered: 232, raceWins: 8, driverChampionships: 0, img1: 'ricciardo.png', img2: 'alpha_tauri.jpg'}
+    {
+        driverName: "Max Verstappen",
+        teamName: "Red Bull Racing",
+        nationality: "Dutch",
+        carNumber: 1,
+        careerPodiums: 87,
+        careerPoints: 2266.5,
+        gpEntered: 173,
+        raceWins: 43,
+        driverChampionships: 2,
+        img1: "verstappen.png",
+        img2: "redbull.jpg"
+    },
+
+    {
+        driverName: "Sergio Perez",
+        teamName: "Red Bull Racing",
+        nationality: "Mexican",
+        carNumber: 11,
+        careerPodiums: 31,
+        careerPoints: 1357,
+        gpEntered: 246,
+        raceWins: 6,
+        driverChampionships: 0,
+        img1: "perez.png",
+        img2: "redbull.jpg"
+    },
+
+    {
+        driverName: "Lewis Hamilton",
+        teamName: "Mercedes",
+        nationality: "British",
+        carNumber: 44,
+        careerPodiums: 195,
+        careerPoints: 4526.5,
+        gpEntered: 320,
+        raceWins: 103,
+        driverChampionships: 7,
+        img1: "hamilton.png",
+        img2: "mercedes.jpg"
+    },
+
+    {
+        driverName: "George Russell",
+        teamName: "Mercedes",
+        nationality: "British",
+        carNumber: 63,
+        careerPodiums: 10,
+        careerPoints: 376,
+        gpEntered: 92,
+        raceWins: 1,
+        driverChampionships: 0,
+        img1: "russell.png",
+        img2: "mercedes.jpg"
+    },
+
+    {
+        driverName: "Charles Leclerc",
+        teamName: "Ferrari",
+        nationality: "Monegasque",
+        carNumber: 16,
+        careerPodiums: 26,
+        careerPoints: 942,
+        gpEntered: 113,
+        raceWins: 5,
+        driverChampionships: 0,
+        img1: "leclerc.png",
+        img2: "ferrari.jpg"},
+
+    {
+        driverName: "Carlos Sainz",
+        teamName: "Ferrari",
+        nationality: "Spanish",
+        carNumber: 55,
+        careerPodiums: 15,
+        careerPoints: 865.5,
+        gpEntered: 173,
+        raceWins: 1,
+        driverChampionships: 0,
+        img1: "sainz.png",
+        img2: "ferrari.jpg"
+    },
+
+    {
+        driverName: "Fernando Alonso",
+        teamName: "Aston Martin",
+        nationality: "Spanish",
+        carNumber: 14,
+        careerPodiums: 104,
+        careerPoints: 2198,
+        gpEntered: 368,
+        raceWins: 32,
+        driverChampionships: 2,
+        img1: "alonso.png",
+        img2: "aston_martin.jpg",
+    },
+
+    {
+        driverName: "Lance Stroll",
+        teamName: "Aston Martin",
+        nationality: "Canadian",
+        carNumber: 18,
+        careerPodiums: 3,
+        careerPoints: 238,
+        gpEntered: 132,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "stroll.png",
+        img2: "aston_martin.jpg"
+    },
+
+    {
+        driverName: "Lando Norris",
+        teamName: "McLaren",
+        nationality: "British",
+        carNumber: 4,
+        careerPodiums: 7,
+        careerPoints: 470,
+        gpEntered: 92,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "norris.png",
+        img2: "mclaren.jpg"
+    },
+
+    {
+        driverName: "Oscar Piastri",
+        teamName: "McLaren",
+        nationality: "Australian",
+        carNumber: 81,
+        careerPodiums: 0,
+        careerPoints: 17,
+        gpEntered: 10,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "piastri.png",
+        img2: "mclaren.jpg"
+    },
+
+    {
+        driverName: "Esteban Ocon",
+        teamName: "Alpine",
+        nationality: "French",
+        carNumber: 31,
+        careerPodiums: 3,
+        careerPoints: 395,
+        gpEntered: 121,
+        raceWins: 1,
+        driverChampionships: 0,
+        img1: "ocon.png",
+        img2: "alpine.jpg"
+    },
+
+    {
+        driverName: "Pierre Gasly",
+        teamName: "Alpine",
+        nationality: "French",
+        carNumber: 10,
+        careerPodiums: 3,
+        careerPoints: 348,
+        gpEntered: 118,
+        raceWins: 1,
+        driverChampionships: 0,
+        img1: "gasly.png",
+        img2: "alpine.jpg"
+    },
+
+    {
+        driverName: "Alexander Albon",
+        teamName: "Williams",
+        nationality: "Thai",
+        carNumber: 23,
+        careerPodiums: 2,
+        careerPoints: 212,
+        gpEntered: 69,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "albon.png",
+        img2: "williams.jpg"
+    },
+
+    {
+        driverName: "Logan Sargeant",
+        teamName: "Williams",
+        nationality: "American",
+        carNumber: 2,
+        careerPodiums: 0,
+        careerPoints: 0,
+        gpEntered: 10,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "sargeant.png",
+        img2: "williams.jpg"
+    },
+
+    {
+        driverName: "Valterri Bottas",
+        teamName: "Alfa Romeo",
+        nationality: "Finnish",
+        carNumber: 77,
+        careerPodiums: 67,
+        careerPoints: 1792,
+        gpEntered: 210,
+        raceWins: 10,
+        driverChampionships: 0,
+        img1: "bottas.png",
+        img2: "alfa_romeo.jpg"
+    },
+
+    {
+        driverName: "Zhou Guanyu",
+        teamName: "Alfa Romeo",
+        nationality: "Chinese",
+        carNumber: 24,
+        careerPodiums: 0,
+        careerPoints: 10,
+        gpEntered: 32,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "zhou.png",
+        img2: "alfa_romeo.jpg"
+    },
+
+    {
+        driverName: "Nico Hulkenberg",
+        teamName: "Haas F1 Team",
+        nationality: "German",
+        carNumber: 27,
+        careerPodiums: 0,
+        careerPoints: 530,
+        gpEntered: 194,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "hulkenberg.png",
+        img2: "haas.jpg"
+    },
+
+    {
+        driverName: "Kevin Magnussesn",
+        teamName: "Haas F1 Team",
+        nationality: "Danish",
+        carNumber: 20,
+        careerPodiums: 1,
+        careerPoints: 185,
+        gpEntered: 152,
+        raceWins: 2,
+        driverChampionships: 0,
+        img1: "magnussen.png",
+        img2: "haas.jpg"
+    },
+
+    {
+        driverName: "Yuki Tsunoda",
+        teamName: "AlphaTauri",
+        nationality: "Japanese",
+        carNumber: 22,
+        careerPodiums: 0,
+        careerPoints: 46,
+        gpEntered: 54,
+        raceWins: 0,
+        driverChampionships: 0,
+        img1: "tsunoda.png",
+        img2: "alpha_tauri.jpg"
+    },
+
+    {
+        driverName: "Daniel Ricciardo",
+        teamName: "AlphaTauri",
+        nationality: "Australian",
+        carNumber: 3,
+        careerPodiums: 32,
+        careerPoints: 1311,
+        gpEntered: 232,
+        raceWins: 8,
+        driverChampionships: 0,
+        img1: "ricciardo.png",
+        img2: "alpha_tauri.jpg"
+    }
 ];
 
 // Player and computer card variables with empty objects
@@ -57,9 +317,9 @@ function dealCards() {
         } else if (playerCardCount === computerCardCount) {
             playerCards.push(randomCard[0]);
             playerCardCount +=1;
-        };
-    };    
-};
+        }
+    } 
+}
 
 // Assign a random card from the players and the computers deck whilst dispolaying the images and the driver stats 
 
@@ -67,15 +327,15 @@ function assignCurrentCard() {
     if (playerCards.length > 0) {
         const playerCurrent = Math.floor((Math.random() * playerCards.length));
         playerCardCurrent.push(playerCards.splice(playerCurrent, 1)[0]);
-    };
+    }
 
     if (computerCards.length > 0) {
         const computerCurrent = Math.floor((Math.random() * computerCards.length));
         computerCardCurrent.push(computerCards.splice(computerCurrent, 1)[0]);
-    };
+    }
     displayImages();
     displayDriverStats();
-};
+}
 
 // Function to display driver and team image details on screen
 
@@ -89,7 +349,7 @@ function displayImages() {
     playerTeamImage.src = `assets/images/team_logos/${playerCardCurrent[0].img2}`;
     computerDriverImage.src = `assets/images/drivers/${computerCardCurrent[0].img1}`;
     computerTeamImage.src = `assets/images/team_logos/${computerCardCurrent[0].img2}`;
-};
+}
 
 //Variables and function to set player and computer driver stats
 
@@ -202,7 +462,7 @@ function determineRoundWinner(outcome) {
 
     const winnerText = playerWin ? "You" : "The Computer";
     const tieMessage = "This round is a tie!";
-    const winnerMessage = `${winnerText} won this round`
+    const winnerMessage = `${winnerText} won this round`;
    
     if(playerWin || computerWin) {
         roundWinnerDisplay.textContent = winnerMessage;
@@ -211,7 +471,7 @@ function determineRoundWinner(outcome) {
     }
     resetMessageDisplay();
 
-    setTimeout( () => {roundWinnerDisplay.style.display = "none"}, 8000);
+    setTimeout( () => {roundWinnerDisplay.style.display = "none";}, 8000);
     playGame();
 }
 
@@ -221,7 +481,7 @@ const resetMessageDisplay = () => roundWinnerDisplay.style.display = "inline-blo
 function playGame() {
         if (playerCards.length < 20 && computerCards.length < 20) {
         assignCurrentCard();
-        return
+        return;
     }
 
     const playerGameWin = playerCards.length === 20;
